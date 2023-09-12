8fdc7e7af1d10067f420884decba2052d0c131ae
zdebski1 <126642683+zdebski1@users.noreply.github.com>
2023-09-12 08:37:24 -0400

    Update diagram.py

--- diagram.py
+++ diagram.py
@@ -10,21 +10,23 @@ from pydantic import BaseModel, Field
 
 
 
-# app = FastAPI()
-# app.include_router(weather_api.router)
+class weather_data(BaseModel): # create a class using pydantics to insure that the zipcode can only be 5 digits long
+    zip_code: str = Field(max_length=5, min_length=5)
+    units: str
+    api_key: str
+
 
 
 async def main():
+    zip_code = input('Zipcode: ')
+    units = input('Unit of measurement: ')
+    api_key = '2cad35927d4cc9b4f8b0d5077090f6dd'  #input('personal token: ')
 
-        zip_code = input('Zipcode: ')
-        units = input('Unit of measurement: ')
-        api_key = input('personal token: ')
-
-        temperature = await weather_api.get_weather_by_zipcode(zip_code, units, api_key)
-        print(temperature)
-
+    data = weather_data(zip_code=zip_code, units=units, api_key=api_key)
+    temperature = await weather_api.get_weather_by_zipcode(data.zip_code, data.units, data.api_key)
+    
+    print(temperature)
 
 if __name__ == "__main__":
-    # Create a loop and run until complete
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
